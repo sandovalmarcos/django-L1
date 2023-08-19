c0bcbedb88a6f793efc7c0ef0e89ac990a388374
Marcos Sandoval <sandovalhmarcos@gmail.com>
2023-08-19 18:52:01 -0400

    tsao

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse('Home')
-
-
-def sobre(request):
-    return HttpResponse('Sobre')
-
-
-def contacto(request):
-    return HttpResponse('Contacto')
+    return render(request, 'recipes/home.html', context={
+        'name': 'Marcos jiji'
+    })
