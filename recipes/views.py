6ea183d2d1af2f243d6b7c2144c6a7cf65ed8465
Marcos Sandoval <sandovalhmarcos@gmail.com>
2023-08-21 00:26:34 -0400

    xzxzx

--- recipes/views.py
+++ recipes/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html', context={
+    return render(request, 'recipes/pages/home.html', context={
         'name': 'Marcos jiji'
     })
