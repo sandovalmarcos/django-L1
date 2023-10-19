0101341f8139b71c71e472a4416d89f32a825ac4
Marcos Sandoval <sandovalhmarcos@gmail.com>
2023-10-18 22:05:16 -0300

    A

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,15 @@
+from utils.recipe.factory import make_recipe
 from django.shortcuts import render
 # Create your views here.
 
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Marcos jiji'
+        'recipes': [make_recipe() for _ in range(9)]
     })
 
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name': 'Marcos jiji'
+        'recipe': make_recipe()
     })
